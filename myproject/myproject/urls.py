9b3c885e15295f5639a084bd29e918005184f0e5
Roeland Strijk <roeland.strijk@gmail.com>
2025-05-23 20:00:39 +0200

    style aanpassing

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from django.conf.urls.static import static 
-from django.conf import settings 
+
 from django.contrib.auth import views as auth_views
 from .views import force_logout 
 
